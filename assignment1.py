ebe7f53004a187b17281640f01b6c60b4d7708e7
TuWeilin <45219165@qq.com>
2020-07-30 17:45:23 +0800

    Update personal particulars

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,12 @@
 """
-Replace the contents of this module docstring with your own details
-Name:
-Date started:
-GitHub URL:
+Name:Tu Weilin
+Date started:30-07-2020
+GitHub URL: https://github.com/JCUS-CP1404/assignment-1-travel-tracker-TuWeilin
 """
 
 
 def main():
+    """Track places visited"""
     print("Travel Tracker 1.0 - by <Your Name>")
 
 
